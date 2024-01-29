5ceed42f469004ed5bc14f48c3e23050c5a62554
vladimirovicp <vladimirovicp70@gmail.com>
2024-01-29 12:12:15 +0400

    feat: основной алгоритм

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const body = document.querySelector('body');
 const wc = 5; //ширина ячеек
 const hc = 5; //высота ячеек
 const cells = wc + hc; // всего ячеек
+let tableValue = [];
+//console.log(tableValue);
+
 
 
 const startHtml = () =>{
@@ -40,24 +43,82 @@ const startHtml = () =>{
 function clickCeil(){
 
     const tableGame = document.querySelector(".table-game");
-    const cells = document.querySelectorAll(".cell");
-
-
+    const cells = tableGame.querySelectorAll(".cell");
 
     cells.forEach( item => {
-        item.addEventListener("click", () => {
-            item.classList.add('checked');
+        item.addEventListener("click", (e) => {
+            if (item.classList.contains('marked')){
+                item.classList.remove('marked');
+            }
+            if (item.classList.contains('checked')){
+                item.classList.remove('checked');
+            } else{
+                item.classList.add('checked');
+                comparisonTable();
+            }
+        })
 
-            //marked
+        item.addEventListener("contextmenu", (e) => {
+            if (item.classList.contains('checked')){
+                item.classList.remove('checked');
+            }
 
+            if (item.classList.contains('marked')){
+                item.classList.remove('marked');
+            } else {
+                item.classList.add('marked');
+            }
+            e.preventDefault();
         })
     })
+}
 
 
+const generateArr = () =>{
+
+    let arr = [];
+    let resArr = [];
+    for (let i = 0; i < wc; i++){
+        for (let j = 0; j < hc; j++){
+            arr[j] = Math.random() > 0.5 ? 1 : 0;
+        }
+        resArr[i] = arr;
+    }
+    return resArr;
+}
+
+const comparisonTable = () =>{
+    const tableGame = document.querySelector(".table-game");
+    const cells = tableGame.querySelectorAll(".cell");
+    let comparisonArr = Array(wc).fill().map(() => Array(hc).fill(0));
+    cells.forEach( item => {
+            if (item.classList.contains('checked')){
+                comparisonArr[item.dataset.col][item.dataset.row] = 1;
+            }
+    });
+
+    // console.log('comparisonArr',comparisonArr);
+    // console.log('tableValue',tableValue)
+
+    if (isEqual(tableValue, comparisonArr)){
+        console.log('Победа!')
+    }
 
 
 }
 
 
+function isEqual(array1, array2) {
+    return JSON.stringify(array1) === JSON.stringify(array2);
+  }
 
-window.onload = startHtml;
+
+
+
+
+window.onload = function() {
+    startHtml();
+    tableValue = generateArr();
+}
+
+
